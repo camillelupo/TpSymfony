57f0bd856e7f00dd3d61e4531f4de7e14950f0c2
Lupo camille <camille.lupo@hotmail.fr>
2020-10-09 10:49:05 +0200

    add faker and form

---
+++ src/Entity/Commune.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommuneRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommuneRepository::class)
+ */
+class Commune
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $codeDepartement;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $codeRegion;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private $codesPostaux = [];
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $population;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getCode(): ?int
+    {
+        return $this->code;
+    }
+
+    public function setCode(int $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getCodeDepartement(): ?int
+    {
+        return $this->codeDepartement;
+    }
+
+    public function setCodeDepartement(int $codeDepartement): self
+    {
+        $this->codeDepartement = $codeDepartement;
+
+        return $this;
+    }
+
+    public function getCodeRegion(): ?int
+    {
+        return $this->codeRegion;
+    }
+
+    public function setCodeRegion(int $codeRegion): self
+    {
+        $this->codeRegion = $codeRegion;
+
+        return $this;
+    }
+
+    public function getCodesPostaux(): ?array
+    {
+        return $this->codesPostaux;
+    }
+
+    public function setCodesPostaux(array $codesPostaux): self
+    {
+        $this->codesPostaux = $codesPostaux;
+
+        return $this;
+    }
+
+    public function getPopulation(): ?int
+    {
+        return $this->population;
+    }
+
+    public function setPopulation(int $population): self
+    {
+        $this->population = $population;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Media.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MediaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MediaRepository::class)
+ */
+class Media
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Commune::class, cascade={"persist", "remove"})
+     */
+    private $commune;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $video;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $image;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $article;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommune(): ?Commune
+    {
+        return $this->commune;
+    }
+
+    public function setCommune(?Commune $commune): self
+    {
+        $this->commune = $commune;
+
+        return $this;
+    }
+
+    public function getVideo(): ?string
+    {
+        return $this->video;
+    }
+
+    public function setVideo(string $video): self
+    {
+        $this->video = $video;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getArticle(): ?string
+    {
+        return $this->article;
+    }
+
+    public function setArticle(?string $article): self
+    {
+        $this->article = $article;
+
+        return $this;
+    }
+}
