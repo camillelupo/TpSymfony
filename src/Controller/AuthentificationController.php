243f53193848d25bf4e64d85311288cd8e24c709
Lupo camille <camille.lupo@hotmail.fr>
2020-10-09 17:36:39 +0200

    final commit

--- src/Controller/AuthentificationController.php
+++ src/Controller/AuthentificationController.php
@@ -6,10 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use OpenApi\Annotations as OA;
 
 class AuthentificationController extends AbstractController
 {
     /**
+     * @OA\Response(
+     *     response=200,
+     *     description="Log the user"
+     * )
      * @Route("/api/login", name="login", methods={"POST"})
      *
      * @param AuthenticationUtils $authenticationUtils
